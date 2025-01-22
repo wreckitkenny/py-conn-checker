c80b11123bbd5ff23450f6cea2c5d9984922a463
Tung Tran Thanh <tung.tran.3295@gmail.com>
2025-01-22 13:23:26 +0700

    First commit

--- k8s/api.py
+++ k8s/api.py
@@ -1,6 +1,7 @@
 from kubernetes import client, config
 
 def get_api():
-    config.load_kube_config()
+    config.load_incluster_config()
+    # config.load_kube_config()
     # v1 = client.CoreV1Api()
     return client
